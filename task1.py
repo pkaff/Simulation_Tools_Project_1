39d502cce485aeb958dded240188ffa7e1d170b8
Linus <mail>
2015-11-10 13:42:17 +0100

    added plots

--- task1.py
+++ task1.py
@@ -7,7 +7,7 @@ from assimulo.solvers import CVode
 from BDF2 import BDF_2
 
 #lambda(y1, y2, k)
-def lambda_func(y1, y2, k = 1):
+def lambda_func(y1, y2, k = -0.1):
     return k*(np.sqrt(y1**2 + y2**2) - 1)/np.sqrt(y1**2 + y2**2)
 
 #the right hand side of our problem
@@ -15,16 +15,15 @@ def rhs(t, y):
     return np.array([y[2], y[3], -y[0]*lambda_func(y[0], y[1]), -y[1]*lambda_func(y[0], y[1]) - 1])
 
 #initial values. y[0] = x-position, y[1] = y-position, y[2] = x-velocity, y[3] = y-velocity
-y0 = np.array([1.0, 0.0, 0.0, -1.0])
+y0 = np.array([1.0, 0.0, 0.0, 0.0])
 t0 = 0.0
 
 #Assimulo stuff
 model = Explicit_Problem(rhs, y0, t0)
 model.name = 'Task 1'
-sim = BDF_2(model)
+sim = CVode(model)
 #sim = CVode(model)
-sim.maxord = 7
-tfinal = 100.0
+tfinal = 3
 #simulation. Store result in t and y
 t, y = sim.simulate(tfinal)
 
@@ -33,17 +32,20 @@ fig, ax = plt.subplots()
 ax.plot(t, y[:, 0], label='x-pos')
 ax.plot(t, y[:, 1], label='y-pos')
 legend = ax.legend(loc='upper center', shadow=True)
+plt.grid()
 
 plt.figure(1)
 fig2, ax2 = plt.subplots()
 ax2.plot(t, y[:, 2], label='x-vel')
 ax2.plot(t, y[:, 3], label='y-vel')
 legend = ax2.legend(loc='upper center', shadow=True)
+plt.grid()
 
 plt.figure(1)
 fig3, ax3 = plt.subplots()
 ax3.plot(y[:, 0], y[:, 1], label='displacement')
 legend = ax3.legend(loc='upper center', shadow=True)
+plt.grid()
 
 # Now add the legend with some customizations.
 plt.show()
