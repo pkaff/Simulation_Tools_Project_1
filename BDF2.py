6acfdf6b7af97479f209869a7c46867f13547019
Linus <mail>
2015-11-10 11:29:01 +0100

    added task 1 and example for bdf2

---
+++ BDF2.py
@@ -0,0 +1,87 @@
+from assimulo.explicit_ode import *
+import numpy as N
+import scipy.linalg as SL
+
+class BDF_2(Explicit_ODE):
+	"""
+	Explicit Euler.
+	"""
+	Tol=1.e-8
+	maxit=100
+		
+	def _integrator(self, t, y, tf, dt):
+		"""
+		_integrates (t,y) values until t > tf
+		"""
+		if dt <= 0.0:
+			raise Explicit_ODE_Exception('BDF-2 is a fixed step-size method. Provide' \
+										 ' the number of communication points.')
+		
+		self.h = dt
+
+		for i in range(self.maxsteps):
+			if t >= tf:
+				break
+			if i==0:  # initial step
+				t_np1,y_np1 = self.step_EE(t,y)
+			else:	
+				t_np1, y_np1 = self.step_BDF2([t,t_nm1], [y,y_nm1])
+			t,t_nm1=t_np1,t
+			y,y_nm1=y_np1,y
+			yield t,y
+			self.h=min(self.h,N.abs(tf-t))
+		else:
+			raise Explicit_ODE_Exception('Final time not reached within maximum number of steps')
+	
+	def step_EE(self, t, y):
+		"""
+		This calculates the next step in the integration with explicit Euler.
+		"""
+		f = self.f
+		h = self.h
+		return t + h, y + h*f(t, y) 
+	def step_BDF2(self,T,Y):
+		"""
+		BDF-2 with Fixed Point Iteration and Zero order predictor
+		
+		alpha_0*y_np1+alpha_1*y_n+alpha_2*y_nm1=h f(t_np1,y_np1)
+		alpha=[3/2,-2,1/2]
+		"""
+		alpha=[3./2.,-2.,1./2]
+		f=self.f
+		h=self.h
+		t_n,t_nm1=T
+		y_n,y_nm1=Y
+		# predictor
+		t_np1=t_n+h
+		y_np1_i=y_n   # zero order predictor
+		# corrector with fixed point iteration
+		for i in range(self.maxit):
+			y_np1_ip1=(-(alpha[1]*y_n+alpha[2]*y_nm1)+h*f(t_np1,y_np1_i))/alpha[0]
+			if SL.norm(y_np1_ip1-y_np1_i) < self.Tol:
+				return t_np1,y_np1_ip1
+			y_np1_i=y_np1_ip1
+		else:
+			raise Explicit_ODE_Exception('Corrector could not converge within % iterations'%i)
+			
+#Define the rhs
+def f(t,y):
+	ydot = -y[0]
+	return N.array([ydot])
+	
+#Define an Assimulo problem
+exp_mod = Explicit_Problem()
+exp_mod.f = f
+exp_mod.problem_name = 'Simple BDF-2 Example'
+
+def pend(t,y):
+	#g=9.81    l=0.7134354980239037
+	gl=13.7503671
+	return N.array([y[1],-gl*N.sin(y[0])])
+pend_mod=Explicit_Problem()
+pend_mod.f=pend
+pend_mod.problem_name='Nonlinear Pendulum'
+pend_mod.y0=N.array([2.*N.pi,0.])
+#Define an explicit solver
+y0 = 4.0 #Initial conditions
+exp_sim = BDF_2(exp_mod,y0) #Create a BDF solver
---
+++ task1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import scipy as sp
+import pylab as pl
+import matplotlib.pyplot as plt
+from assimulo.problem import Explicit_Problem
+from assimulo.solvers import CVode
+
+def lambda_func(y1, y2, k = 1):
+    return k*(np.sqrt(y1**2 + y2**2) - 1)/np.sqrt(y1**2 + y2**2)
+
+def rhs(t, y):
+    return np.array([y[2], y[3], -y[0]*lambda_func(y[0], y[1]), -y[1]*lambda_func(y[0], y[1]) - 1])
+
+y0 = np.array([1.0, 0.0, 0.0, -1.0])
+t0 = 0.0
+
+model = Explicit_Problem(rhs, y0, t0)
+model.name = 'Task 1'
+sim = CVode(model)
+sim.maxord = 7
+tfinal = 100.0
+t, y = sim.simulate(tfinal)
+#pl.plot(y[:, 0], y[:, 1])
+#pl.show()
+#sim.plot()
+fig, ax = plt.subplots()
+ax.plot(t, y[:, 0], label='x-pos')
+ax.plot(t, y[:, 1], label='y-pos')
+legend = ax.legend(loc='upper center', shadow=True)
+
+plt.figure(1)
+fig2, ax2 = plt.subplots()
+ax2.plot(t, y[:, 2], label='x-vel')
+ax2.plot(t, y[:, 3], label='y-vel')
+legend = ax2.legend(loc='upper center', shadow=True)
+
+plt.figure(1)
+fig3, ax3 = plt.subplots()
+ax3.plot(y[:, 0], y[:, 1], label='displacement')
+legend = ax3.legend(loc='upper center', shadow=True)
+
+#ax.plot(a, c, 'k--', label='Model length')
+#ax.plot(a, d, 'k:', label='Data length')
+#ax.plot(a, c+d, 'k', label='Total message length')
+
+# Now add the legend with some customizations.
+plt.show()
